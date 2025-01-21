1bc6b311ec64cd6008b0a60ecaef21537af8e798
Nikolai <nikolayb33@gmail.com>
2025-01-21 19:50:50 +0000

    add end point get book

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+
+books = [
+    {
+        "id": 1,
+        "title": "Labirint",
+        "autor": "Lukyanenko",
+    },
+    {
+        "id": 2,
+        "title": "Sbornik",
+        "autor": "Turgenev",
+    }
+]
+
+@app.get("/")
+def home():
+    return ("Hello world!!!!")
+
+# получить все книги
+@app.get("/books")
+def read_books():
+    return books
+
+# получить книгу по id
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException
 
 if __name__ == "__main__":
-    print("hell0")
+    uvicorn.run("main:app", reload=True)
+
